6125e244e3ea5cde76b62309105346bd31971d6b
Pranjal Bansal <pranban12@gmail.com>
2021-06-18 08:23:57 +0530

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,22 @@
+var output =[];
+ 
+function fizbuzz(){
+for (var  count= 1; count<101; count++) {
+if(count % 3===0 && count %5===0) {
+output.push("fizbuzz");
+}
+else if (count %3===0)
+{
+output.push("Fizz");
+}
+else if(count %5===0)
+{
+output.push("Buzz")
+}
+else
+{
+output.push(count);
+}
+}
+console.log(output);
+}
